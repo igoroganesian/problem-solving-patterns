dd050d334d02cfd317dcb3e066accac7f9fc5306
Igor Oganesian <igor.oganesian@gmail.com>
2023-07-07 18:37:43 -0400

    completed can-construct-word

--- can-construct-word.js
+++ can-construct-word.js
@@ -1,3 +1,27 @@
-// add whatever parameters you deem necessary & write doc comment
-function canConstructWord() {
+"use strict";
+
+function canConstructWord(word, letters) {
+  const letterCache = freqCounter(letters);
+  const wordCache = freqCounter(word);
+
+  for (let word in wordCache) {
+    if (!letterCache[word]) {
+      return false;
+    }
+    if (wordCache[word] > letterCache[word]) {
+      return false;
+    }
+  }
+  return true;
+ }
+
+function freqCounter(string) {
+  const freqs = {};
+
+  for (const letter of string) {
+    const curr = freqs[letter] || 0;
+    freqs[letter] = curr + 1;
+  }
+  return freqs;
 }
+
