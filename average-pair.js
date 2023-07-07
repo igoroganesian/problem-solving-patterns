bb9d02d50f80eae57b33e6b1b861a9b1941d4c7f
Igor Oganesian <igor.oganesian@gmail.com>
2023-07-07 19:13:18 -0400

    copied average-pair.js to resolve git disconnect

--- average-pair.js
+++ average-pair.js
@@ -1,4 +1,26 @@
-// add whatever parameters you deem necessary & write docstring
-function averagePair() {
+"use strict";
+
+/** takes in an array nums, and a targetAvg
+ * returns a boolean if the avg can be made from array contents
+ */
+function averagePair(nums, targetAvg) {
+
+let left = 0;
+let right = nums.length -1;
+
+while (left < right) {
+  const average = (nums[left] + nums[right]) / 2;
+
+  if (average === targetAvg) {
+    return true;
+  } else if (average > targetAvg) {
+    right--;
+  } else {
+    left++;
+  }
+}
+
+throw new Error ("average not found");
+
 }
 
