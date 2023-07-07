4249e391a13d212b3f2db8a1386220dddc1e33ae
jesse.chung9727@gmail.com <jesse.chung9727@gmail.com>
2023-07-07 19:54:50 -0400

    completed sameFrequency

--- same-frequency.js
+++ same-frequency.js
@@ -1,3 +1,19 @@
-// add whatever parameters you deem necessary & write docstring
-function sameFrequency() {
+"use strict";
+
+/** accepts two numbers as input, and returns a boolean depending on
+ * if the two numbers have the same frequency of digits
+ */
+function sameFrequency(num1, num2) {
+
+  let str1 = num1.toString().split('').sort();
+  let str2 = num2.toString().split('').sort();
+
+  if (str1.length !== str2.length) return false;
+
+  for (let i = 0; i < str1.length; i++){
+    if (str1[i] !== str2[i]){
+      return false;
+    }
+  }
+  return true;
 }
