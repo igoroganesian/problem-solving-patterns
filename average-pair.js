c2b4f6f0ece7598da8e5a5b03e8d210c99777ebb
Igor Oganesian <igor.oganesian@gmail.com>
2023-07-07 19:34:40 -0400

    solved two-array-object

--- average-pair.js
+++ average-pair.js
@@ -5,21 +5,20 @@
  */
 function averagePair(nums, targetAvg) {
 
-let left = 0;
-let right = nums.length -1;
+  let left = 0;
+  let right = nums.length - 1;
 
-while (left < right) {
-  const average = (nums[left] + nums[right]) / 2;
+  while (left < right) {
+    const average = (nums[left] + nums[right]) / 2;
 
-  if (average === targetAvg) {
-    return true;
-  } else if (average > targetAvg) {
-    right--;
-  } else {
-    left++;
+    if (average === targetAvg) {
+      return true;
+    } else if (average > targetAvg) {
+      right--;
+    } else {
+      left++;
+    }
   }
-}
-
-return false;
+  return false;
 }
 
--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,22 @@
-// add whatever parameters you deem necessary & write docstring
-function twoArrayObject() {
+"use strict";
+
+/** accepts two arrays ([keys], [values]); returns object of keys:values
+ * if not enough values, excess keys set to null
+ * if not enough keys, ignore excess values
+ */
+
+function twoArrayObject(keys, values) {
+  const outputObject = {};
+
+  for (let i = 0; i < keys.length; i++) {
+    if (!values[i]) {
+      outputObject[keys[i]] = null;
+    } else {
+      outputObject[keys[i]] = values[i];
+    }
+  }
+
+  return outputObject;
 }
+
+//possible to refactor?
